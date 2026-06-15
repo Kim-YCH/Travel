9c04f770b45db7b3beaafc913aff86dba53155be
Kim-YCH <sss881015@gmail.com>
2026-06-15 17:55:17 +0800

    Split app and add hotel edit sync queue

---
+++ config.js
@@ -0,0 +1,4 @@
+window.TRAVEL_CONFIG = Object.freeze({
+  API_URL: 'https://script.google.com/macros/s/AKfycbyp7AnyszddlubQRfQDg5HpqBIL6PMuvCpzf__iCw-0Zb0LNyNe191qNDc-fjnN7QQ/exec',
+  GOOGLE_MAPS_API_KEY: 'AIzaSyCLrHk9V-eQby0aDVx31iwFyqmhI-jIs4Q'
+});
