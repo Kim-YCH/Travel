3d783f52901ea0e2679b779c5807c69894ba4d93
Kim-YCH <sss881015@gmail.com>
2026-06-24 09:24:32 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.TRAVEL_CONFIG = Object.freeze({
-  API_URL: 'https://script.google.com/macros/s/AKfycbyp7AnyszddlubQRfQDg5HpqBIL6PMuvCpzf__iCw-0Zb0LNyNe191qNDc-fjnN7QQ/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxxhXeQz56i7l0Qj6k-l-9EgkDuYLCoDuHynigq-66sZrxD1s_yRAA02KS-iUmNYBKH/exec',
   GOOGLE_MAPS_API_KEY: 'AIzaSyCLrHk9V-eQby0aDVx31iwFyqmhI-jIs4Q',
   APP_VERSION: '20260623.02'
 });
