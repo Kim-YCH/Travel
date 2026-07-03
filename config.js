4d2b95fdff5fd2a9f92b11a5f6a5eea5f8cfa1bf
Kim-YCH <sss881015@gmail.com>
2026-07-03 15:23:37 +0800

    20260703.4 Apply Cloud Blue theme

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.TRAVEL_CONFIG = Object.freeze({
   API_URL: 'https://script.google.com/macros/s/AKfycbz63QJeYWb_Hpiv3KXBnl3St-1FJrT6gqNDPEuDECygphYNA-Rko2ED52859cMO5whY/exec',
   GOOGLE_MAPS_API_KEY: 'AIzaSyCLrHk9V-eQby0aDVx31iwFyqmhI-jIs4Q',
-  APP_VERSION: '20260703.3'
+  APP_VERSION: '20260703.4'
 });
