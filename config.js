c06103d68c0cb30ae89173ad5e4252e87b1b5de7
Kim-YCH <sss881015@gmail.com>
2026-07-03 11:50:08 +0800

    20260703.3 Fix checklist owner sync

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.TRAVEL_CONFIG = Object.freeze({
-  API_URL: 'https://script.google.com/macros/s/AKfycbzf4ykLrOwMuVO7NT4_r4lUPDlAS55fWJ_Vc1YOIEmvUKybozTifdJFhMacwWnQe39Q/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbz63QJeYWb_Hpiv3KXBnl3St-1FJrT6gqNDPEuDECygphYNA-Rko2ED52859cMO5whY/exec',
   GOOGLE_MAPS_API_KEY: 'AIzaSyCLrHk9V-eQby0aDVx31iwFyqmhI-jIs4Q',
-  APP_VERSION: '20260703.2'
+  APP_VERSION: '20260703.3'
 });
