5a6f83c55ed4491eb1fad1bb08b602de982a3c2e
Kim_YCH <sss881015@gmail.com>
2026-07-05 15:14:53 +0800

    Update config API URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.TRAVEL_CONFIG = Object.freeze({
-  API_URL: 'https://script.google.com/macros/s/AKfycbyPrHuK6cws9v6d5yk08LBZi5EWErUFtn4UuOvQMns_r5KpgoetJMoytzTHVWMtRZEE/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbw7CGAtQHIjAgsTXJlRyQ3f6Nk6PaGFseunjK4Asy--bqyGO0TshHmafECXj7QPDbc/exec',
   GOOGLE_MAPS_API_KEY: 'AIzaSyCLrHk9V-eQby0aDVx31iwFyqmhI-jIs4Q',
   APP_VERSION: '20260705.6'
 });
