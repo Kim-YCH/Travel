bb7fa405fab06b88cf759c298f9f4fe0c2f9c1cc
Kim_YCH <sss881015@gmail.com>
2026-09-13 00:37:28 +0800

    Publish v20260913.4: update Apps Script endpoint

--- config.js
+++ config.js
@@ -8,7 +8,7 @@
  * localStorage.removeItem('travel_debug_enabled'); location.reload();
  */
 window.TRAVEL_CONFIG = Object.freeze({
-  API_URL: 'https://script.google.com/macros/s/AKfycbzFXM2KmNgKtLmM2bjUrppxxdCRX8lE8W1d5e2kDjObsZfBn27HF34fjDBGP7S1bgS0/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxTzTn-2MYmyeEpmaOrGF06y2TIeNzTFzOFsmYn1CiMMWifq8X7IdCP9lS2rFxI5BY/exec',
   GOOGLE_MAPS_API_KEY: 'AIzaSyCLrHk9V-eQby0aDVx31iwFyqmhI-jIs4Q',
-  APP_VERSION: '20260913.3'
+  APP_VERSION: '20260913.4'
 });
