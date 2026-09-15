d22510085d53d8e7e822ebf899f83765a182be68
Kim_YCH <sss881015@gmail.com>
2026-09-15 20:46:31 +0800

    Publish v20260915.2: show personal expense currency

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  */
 'use strict';
 
-const VERSION = '20260915.1';
+const VERSION = '20260915.2';
 const SHELL_CACHE = `travel-shell-${VERSION}`;
 const PREP_IMAGE_CACHE = 'travel-prep-images-v1';
 const PREP_IMAGE_CACHE_LIMIT = 60;
